585fdb8751f12fd314ab186011d5bd768fe4863d
Brian Riley <riley.bri@gmail.com>
2016-01-14 13:20:07 -0500

    Added missing Writer to server.js

--- public/js/mft.js
+++ public/js/mft.js
@@ -1,5 +1,5 @@
 $(function(){	
-	var socket = io.connect('http://0.0.0.0:5001');
+	var socket = io.connect('http://0.0.0.0:');
   
   socket.on('connect', function(){
     var delivery = new Delivery(socket);
@@ -14,8 +14,6 @@ $(function(){
 				for(var i = 0; i < numFiles; i++){
 					var file = input[0].files[i];
 					var params = {foo: "bar"};
-					
-					alert(file);
 					
 					delivery.send(file, params);
 					evt.preventDefault();
